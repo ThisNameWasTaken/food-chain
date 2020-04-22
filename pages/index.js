cf498b8210f53db00eaa8c7642823749a1f7ec2c
Adrian <adriandinca2008@yahoo.com>
2020-04-23 00:20:03 +0300

    feat: Add button actions

--- pages/index.js
+++ pages/index.js
@@ -9,16 +9,16 @@ import {
 } from '@material-ui/core';
 
 import { useForm } from 'react-hook-form';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-import web3 from 'web3';
-import cryptoJs from 'crypto-js';
+import Web3 from 'web3';
+import * as CryptoJS from 'crypto-js';
 
 const useStyles = makeStyles(theme => ({
   container: {
     maxWidth: 400,
     display: 'flex',
-    height: '100vh',
+    height: 'calc(var(--vh, 1vh) * 100)',
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -42,7 +42,7 @@ const useStyles = makeStyles(theme => ({
 const Home = () => {
   const styles = useStyles();
 
-  const { register, handleSubmit, errors } = useForm();
+  const { register, handleSubmit, errors, setValue, getValues } = useForm();
   const onSubmit = data => {
     console.log(locationAction);
     console.log(data);
@@ -50,29 +50,99 @@ const Home = () => {
 
   const [locationAction, setLocationAction] = useState(null);
 
-  const web3 = new Web3(Web3.givenProvider);
-  var foodChainABI, foodChainContract, acc;
+  // /** @type {Eth.Contract} */
+  let foodChainContract;
+  let foodChainCode;
+  let account;
+  let foodChainABI;
+  let web3;
 
-const start = () => {
-  web3.eth.getAccounts(function(err, accounts) {
-    account = accounts[0];
-    web3.eth.defaultAccount = acc;
-  })
-  const http = new XMLHttpRequest();
-  const url = 'http://localhost:8000';
-  http.open("GET", url);
-  http.send();
-  http.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200)
-    {
-      var output = JSON.parse(this.response);
-      foodChainCode = output.contracts[":FoodChain"].bytecode;
-      var metadata = JSON.parse(output.contracts[":FoodChain"].metadata);
-      foodChainABI = metadata.output.abi;
+  useEffect(() => {
+    web3 = new Web3(Web3.givenProvider);
+
+    console.log(web3);
+
+    (async function start() {
+      web3.eth.getAccounts(function (err, accounts) {
+        if (err) throw err;
+
+        console.log({ accounts });
+
+        account = accounts[0];
+        web3.eth.defaultAccount = account;
+      });
+
+      const res = await fetch('http://localhost:3001');
+      const data = JSON.parse(await res.text());
+      console.log(data);
+      foodChainCode = data.contracts.contract.FoodChain.evm.bytecode;
+      foodChainABI = data.contracts.contract.FoodChain.abi;
       foodChainContract = new web3.eth.Contract(foodChainABI);
-    }
+    })();
+  }, []);
+
+  async function createContract() {
+    const deployedContract = await foodChainContract
+      .deploy({ data: foodChainCode })
+      .send({ from: account, data: foodChainCode, gas: 3000000 })
+      .on('confirmation', function (confirmationNumber, receipt) {});
+
+    const contractAddress = deployedContract.options.address;
+    setValue('Contract Address', contractAddress);
   }
-}
+
+  function addNewLocation() {
+    const formData = getValues();
+    const contractAddress = formData['Contract Address'];
+
+    const deployedFoodChain = new web3.eth.Contract(
+      foodChainABI,
+      contractAddress,
+      { from: account, gas: 3000000 }
+    );
+
+    const locationId = formData['Location ID'];
+    const locationName = formData['Location Name'];
+    const locationSecret = formData['Secret'];
+    const passPhrase = formData['Pass Phrase'];
+
+    const encryptedSecret = CryptoJS.AES.encrypt(
+      locationSecret,
+      passPhrase
+    ).toString();
+
+    deployedFoodChain.methods
+      .AddNewLocation(locationId, locationName, encryptedSecret)
+      .send({ from: account });
+  }
+
+  async function getCurrentLocation() {
+    const formData = getValues();
+    const contractAddress = formData['Contract Address'];
+
+    const deployedFoodChain = new web3.eth.Contract(
+      foodChainABI,
+      contractAddress,
+      { from: web3.eth.defaultAccount, gas: 3000000 }
+    );
+
+    const passPhrase = formData['Pass Phrase'];
+    const trailCount = await deployedFoodChain.methods.GetTrailCount().call();
+    const returnValues = await deployedFoodChain.methods
+      .GetLocation(trailCount - 1)
+      .call();
+
+    setValue('Location Name', returnValues[0]);
+    setValue('Location ID', returnValues[1]);
+
+    const encryptedSecret = returnValues[4];
+    const decryptedSecret = CryptoJS.AES.decrypt(
+      encryptedSecret,
+      passPhrase
+    ).toString(CryptoJS.enc.Utf8);
+
+    setValue('Secret', decryptedSecret);
+  }
 
   return (
     <div className="container">
@@ -86,8 +156,8 @@ const start = () => {
                     <TextField
                       className={styles.textField}
                       variant="outlined"
-                      label="Contact Address"
-                      name="Contact Address"
+                      label="Contract Address"
+                      name="Contract Address"
                       inputRef={register({ required: true })}
                     />
                   </Grid>
@@ -129,28 +199,46 @@ const start = () => {
                   </Grid>
                 </Grid>
 
-                <Grid className={styles.buttonsContainer} container spacing={1}>
-                  <Grid item xs>
+                <Grid
+                  className={styles.buttonsContainer}
+                  container
+                  spacing={1}
+                  wrap="wrap"
+                >
+                  <Grid item xs={12}>
                     <Button
                       className={styles.button}
                       variant="contained"
                       color="primary"
                       type="submit"
                       onClick={event => {
-                        setLocationAction('add');
+                        createContract();
+                      }}
+                    >
+                      Create Contract
+                    </Button>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Button
+                      className={styles.button}
+                      variant="contained"
+                      color="primary"
+                      type="submit"
+                      onClick={event => {
+                        addNewLocation();
                       }}
                     >
                       Add location
                     </Button>
                   </Grid>
-                  <Grid item xs>
+                  <Grid item xs={6}>
                     <Button
                       className={styles.button}
                       variant="contained"
                       color="secondary"
                       type="submit"
                       onClick={event => {
-                        setLocationAction('get');
+                        getCurrentLocation();
                       }}
                     >
                       Get location
