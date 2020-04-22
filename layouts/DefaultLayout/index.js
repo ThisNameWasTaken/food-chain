e003faf9b8fc35ec335adb725e472a856995e3e0
Adrian <adriandinca2008@yahoo.com>
2020-04-23 00:19:44 +0300

    fix: Calculate vh units properly

--- layouts/DefaultLayout/index.js
+++ layouts/DefaultLayout/index.js
@@ -1,93 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-
-import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
-import {
-  Home as HomeIcon,
-  Person as ProfileIcon,
-  Search as SearchIcon,
-} from '@material-ui/icons';
-
-import { useStyles } from './styles';
+import React, { useEffect } from 'react';
 
 const DefaultLayout = ({ children }) => {
-  const styles = useStyles();
-
-  const routes = [
-    {
-      label: 'home',
-      path: '/home',
-      icon: <HomeIcon />,
-    },
-    {
-      label: 'profile',
-      path: '/profile',
-      icon: <ProfileIcon />,
-    },
-    {
-      label: 'search',
-      path: '/search',
-      icon: <SearchIcon />,
-    },
-  ];
-
-  const routesWithBottomNavigation = ['/home', '/profile', '/search', '/post'];
-
-  const router = useRouter();
-
-  const [activeRoute, setActiveRoute] = useState(null);
-  const [isScrollingDown, setIsScrollingDown] = useState(false);
-  const [shouldRenderNavigation, setShouldRenderNavigation] = useState(false);
-
-  const onRouteChange = (event, newValue) => {
-    setActiveRoute(newValue);
-
-    router.push(newValue);
-  };
-
   useEffect(() => {
-    setActiveRoute(router.route);
+    function calculateViewPortUnits() {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
 
-    setShouldRenderNavigation(
-      routesWithBottomNavigation.includes(router.route)
-    );
+    calculateViewPortUnits();
+    window.addEventListener('resize', calculateViewPortUnits);
 
-    window.addEventListener('scroll', function (event) {
-      // @ts-ignore
-      setIsScrollingDown(this.prevScrollY < this.scrollY);
-      // @ts-ignore
-      this.prevScrollY = this.scrollY;
-    });
-
-    routes.forEach(({ path }) => {
-      router.prefetch(path);
-    });
+    return function cleanUp() {
+      window.removeEventListener('resize', calculateViewPortUnits);
+    };
   }, []);
 
-  return (
-    <>
-      <>{children}</>
-      {/* TODO: Export this component and lazy load it */}
-      {shouldRenderNavigation && (
-        <BottomNavigation
-          className={`${styles.bottomNavigation} ${
-            isScrollingDown && styles.bottomNavigationHidden
-          }`}
-          value={activeRoute}
-          onChange={onRouteChange}
-        >
-          {routes.map(({ path, label, icon }) => (
-            <BottomNavigationAction
-              key={path}
-              value={path}
-              label={label}
-              icon={icon}
-            />
-          ))}
-        </BottomNavigation>
-      )}
-    </>
-  );
+  return <>{children}</>;
 };
 
 export default DefaultLayout;
