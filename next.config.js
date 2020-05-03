6c3fbd283686643e3534a16d1f1f12d53ddba1a7
Alex Simion <simion.alexgabriel@yahoo.com>
2020-05-03 13:26:14 +0300

    Fixed everything

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
-require('dotenv').config({ path: `.env.${process.env.NODE_ENV || ''}` });
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV || ""}` });
 
-const env = Object.keys(process.env)
-  .filter(key => !key.startsWith('NODE_'))
+const env = Object.keys("")
+  .filter(key => !key.startsWith("NODE_"))
   .reduce(
     (env, key) => ({
       ...env,
-      [key]: process.env[key],
+      [key]: process.env[key]
     }),
     {}
   );
 
-const withCss = require('@zeit/next-css');
-const withPreact = require('next-preactx-plugin');
+const withCss = require("@zeit/next-css");
+const withPreact = require("next-preactx-plugin");
 
 module.exports = withCss(
   withPreact({
-    env,
+    env
   })
 );
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import {
   CardContent,
   Card,
   Grid,
+  Typography,
   makeStyles
 } from "@material-ui/core";
 
@@ -26,7 +27,8 @@ const useStyles = makeStyles(theme => ({
     alignItems: "center"
   },
   card: {
-    width: "100%"
+    width: "100%",
+    marginBottom: theme.spacing(1)
   },
   textField: {
     width: "100%",
@@ -42,11 +44,23 @@ const useStyles = makeStyles(theme => ({
   },
   createContractButton: {
     background: "#73BFB8"
+  },
+  locationInfo: {
+    padding: theme.spacing(0.5, 1)
   }
 }));
 
+function BottomCard() {}
+
 const Home = () => {
   const styles = useStyles();
+
+  const [account, setAccount] = useState(null);
+  const [locationDetails, setLocationDetails] = useState({
+    id: "",
+    name: "",
+    secret: ""
+  });
 
   const { register, handleSubmit, errors, setValue, getValues } = useForm();
   const onSubmit = data => {
@@ -54,260 +68,248 @@ const Home = () => {
   };
 
   // /** @type {Eth.Contract} */
-  let foodChainContract;
-  let foodChainCode;
-  let account;
-  let foodChainABI;
-  let web3;
+  let web3 = null;
+  let contract = null;
+
+  async function initWeb3() {
+    // @ts-ignore
+    if (window.ethereum) {
+      // @ts-ignore
+      await window.ethereum.enable();
+      // @ts-ignore
+      return new Web3(window.ethereum);
+      // @ts-ignore
+    } else if (window.web3) {
+      // @ts-ignore
+      return new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
+      return null;
+    }
+  }
+
+  async function getNetworkData() {
+    const id = await web3.eth.net.getId();
+
+    const networkId = id;
+    console.log("NETWORKID", networkId, FoodChain.networks[networkId]);
+    return networkId ? FoodChain.networks[networkId] : null;
+  }
+
+  async function getContract() {
+    if (!!contract) {
+      return contract;
+    } else {
+      if (!web3) {
+        web3 = await initWeb3();
+      }
+      const networkData = await getNetworkData();
+
+      console.log(networkData);
+
+      contract = new web3.eth.Contract(FoodChain.abi, networkData.address);
+      return contract;
+    }
+  }
 
   useEffect(() => {
-    console.log(Web3.givenProvider);
-    console.log(window.ethereum);
-
-    (async function start() {
-      web3 = await (async function initWeb3() {
-        // @ts-ignore
-        if (window.ethereum) {
-          // @ts-ignore
-          await window.ethereum.enable();
-          // @ts-ignore
-          return new Web3(window.ethereum);
-          // @ts-ignore
-        } else if (window.web3) {
-          // @ts-ignore
-          return new Web3(window.web3.currentProvider);
-        } else {
-          window.alert(
-            "Non-Ethereum browser detected. You should consider trying MetaMask!"
-          );
-          return null;
-        }
-      })();
+    (async () => {
+      web3 = await initWeb3();
 
       web3.eth.getAccounts(function(err, accounts) {
         if (err) throw err;
-
-        console.log({ accounts });
-
-        account = accounts[0];
+        setAccount(accounts[0]);
         web3.eth.defaultAccount = account;
       });
 
-      console.log(FoodChain);
-      foodChainCode = FoodChain.bytecode;
-      foodChainABI = FoodChain.abi;
-      foodChainContract = new web3.eth.Contract(foodChainABI);
+      // const latest = await web3.eth.getBlockNumber();
+
+      // for (var i = 0; i < 10; i++) {
+      //   web3.eth.getBlock(latest - i).then(console.log);
+      // }
     })();
   }, []);
 
-  let _contract;
-
-  async function getTrailCount() {
-    async function getNetworkData() {
-      const id = await web3.eth.net.getId();
-
-      const networkId = id;
-      return networkId ? FoodChain.networks[networkId] : null;
-    }
-
-    async function getContract() {
-      if (_contract) {
-        return _contract;
-      } else {
-        if (!web3) {
-          return null;
-        }
-        const networkData = await getNetworkData();
-        console.log({ networkData });
-        if (!networkData) {
-          return null;
-        }
-
-        _contract = new web3.eth.Contract(FoodChain.abi, networkData.address);
-        console.log({ _contract });
-        return _contract;
-      }
-    }
-
-    const contract = await getContract();
-    console.log({ contract });
-    const trailCCount = await contract.methods.GetTrailCount().call();
-    console.log("trail count", trailCCount);
-    // console.log('trail count', contract.methods.GetTrailCount());
-    return null;
-  }
-
-  async function createContract() {
-    console.log(foodChainCode);
-
-    const deployedContract = await foodChainContract
-      .deploy({ data: foodChainCode })
-      .send({ from: account, data: foodChainCode, gas: 3000000 })
-      .on("confirmation", function(confirmationNumber, receipt) {});
-
-    const contractAddress = deployedContract.options.address;
-    setValue("Contract Address", contractAddress);
-  }
-
-  function addNewLocation() {
+  async function addNewLocation() {
     const formData = getValues();
-    const contractAddress = formData["Contract Address"];
-
-    const deployedFoodChain = new web3.eth.Contract(
-      foodChainABI,
-      contractAddress,
-      { from: account, gas: 3000000 }
-    );
-
     const locationId = formData["Location ID"];
     const locationName = formData["Location Name"];
     const locationSecret = formData["Secret"];
     const passPhrase = formData["Pass Phrase"];
+
+    const deployedFoodChain = await getContract();
 
     const encryptedSecret = CryptoJS.AES.encrypt(
       locationSecret,
       passPhrase
     ).toString();
 
-    deployedFoodChain.methods
+    if (account == null) {
+      web3.eth.getAccounts(function(err, accounts) {
+        if (err) throw err;
+        // account = accounts[0];
+        setAccount(accounts[0]);
+        web3.eth.defaultAccount = account;
+      });
+    }
+
+    console.log({ account });
+
+    const result = await deployedFoodChain.methods
       .AddNewLocation(locationId, locationName, encryptedSecret)
-      .send({ from: account });
+      .send({ from: account, gas: 200000 });
   }
 
   async function getCurrentLocation() {
     const formData = getValues();
-    const contractAddress = formData["Contract Address"];
+    console.log({ formData });
+    const passPhrase = formData["Pass Phrase Copy"];
 
-    const deployedFoodChain = new web3.eth.Contract(
-      foodChainABI,
-      contractAddress,
-      { from: web3.eth.defaultAccount, gas: 3000000 }
-    );
+    console.log({ passPhrase });
 
-    const passPhrase = formData["Pass Phrase"];
+    const deployedFoodChain = await getContract();
+
     const trailCount = await deployedFoodChain.methods.GetTrailCount().call();
-    const returnValues = await deployedFoodChain.methods
+    const returnedValues = await deployedFoodChain.methods
       .GetLocation(trailCount - 1)
       .call();
 
-    setValue("Location Name", returnValues[0]);
-    setValue("Location ID", returnValues[1]);
+    console.log({ returnedValues });
 
-    const encryptedSecret = returnValues[4];
+    const encryptedSecret = returnedValues[4];
     const decryptedSecret = CryptoJS.AES.decrypt(
       encryptedSecret,
       passPhrase
     ).toString(CryptoJS.enc.Utf8);
 
-    setValue("Secret", decryptedSecret);
+    console.log({ encryptedSecret });
+    console.log({ decryptedSecret });
+
+    setLocationDetails({
+      id: returnedValues[1],
+      name: returnedValues[0],
+      secret: decryptedSecret
+    });
   }
 
   return (
     <div className="container">
       <main>
+        {/* M-a scos de pe skype Am vzt:)) suna ma cand intri */}
         <Container className={styles.container}>
-          <Card className={styles.card}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <CardContent>
-                <Grid container direction="column">
-                  <Grid item>
-                    <TextField
-                      className={styles.textField}
-                      variant="outlined"
-                      label="Contract Address"
-                      name="Contract Address"
-                      inputRef={register({ required: true })}
-                    />
+          <Grid container direction="row">
+            <Card className={styles.card}>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <CardContent>
+                  <Grid container direction="column">
+                    <Grid item>
+                      <TextField
+                        className={styles.textField}
+                        variant="outlined"
+                        label="Location ID"
+                        name="Location ID"
+                        inputRef={register()}
+                      />
+                    </Grid>
+                    <Grid item>
+                      <TextField
+                        className={styles.textField}
+                        variant="outlined"
+                        label="Location Name"
+                        name="Location Name"
+                        inputRef={register()}
+                      />
+                    </Grid>
+                    <Grid item>
+                      <TextField
+                        className={styles.textField}
+                        variant="outlined"
+                        label="Secret"
+                        name="Secret"
+                        inputRef={register()}
+                      />
+                    </Grid>
+                    <Grid item>
+                      <TextField
+                        className={styles.textField}
+                        variant="outlined"
+                        label="Pass Phrase"
+                        name="Pass Phrase"
+                        inputRef={register()}
+                      />
+                    </Grid>
                   </Grid>
-                  <Grid item>
-                    <TextField
-                      className={styles.textField}
-                      variant="outlined"
-                      label="Location ID"
-                      name="Location ID"
-                      inputRef={register({ required: true })}
-                    />
-                  </Grid>
-                  <Grid item>
-                    <TextField
-                      className={styles.textField}
-                      variant="outlined"
-                      label="Location Name"
-                      name="Location Name"
-                      inputRef={register({ required: true })}
-                    />
-                  </Grid>
-                  <Grid item>
-                    <TextField
-                      className={styles.textField}
-                      variant="outlined"
-                      label="Secret"
-                      name="Secret"
-                      inputRef={register({ required: true })}
-                    />
-                  </Grid>
-                  <Grid item>
-                    <TextField
-                      className={styles.textField}
-                      variant="outlined"
-                      label="Pass Phrase"
-                      name="Pass Phrase"
-                      inputRef={register({ required: true })}
-                    />
-                  </Grid>
-                </Grid>
 
-                <Grid
-                  className={styles.buttonsContainer}
-                  container
-                  spacing={1}
-                  wrap="wrap"
-                >
-                  <Grid item xs={12}>
-                    <Button
-                      className={styles.button}
-                      variant="contained"
-                      color="primary"
-                      type="submit"
-                      onClick={event => {
-                        createContract();
-                      }}
-                    >
-                      Create Contract
-                    </Button>
+                  <Grid
+                    className={styles.buttonsContainer}
+                    container
+                    spacing={1}
+                    wrap="wrap"
+                  >
+                    <Grid item xs={12}>
+                      <Button
+                        className={styles.button}
+                        variant="contained"
+                        color="primary"
+                        type="submit"
+                        onClick={addNewLocation}
+                      >
+                        Add location
+                      </Button>
+                    </Grid>
                   </Grid>
-                  <Grid item xs={6}>
-                    <Button
-                      className={styles.button}
-                      variant="contained"
-                      color="primary"
-                      type="submit"
-                      onClick={async event => {
-                        // addNewLocation();
-                        // console.log('trailcount', await getTrailCount());
-                        getTrailCount();
-                      }}
-                    >
-                      Add location
-                    </Button>
+                </CardContent>
+              </form>
+            </Card>
+            <Card className={styles.card}>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <CardContent>
+                  <Grid container direction="column">
+                    <Grid item>
+                      <TextField
+                        className={styles.textField}
+                        variant="outlined"
+                        label="Pass Phrase Copy"
+                        name="Pass Phrase Copy"
+                        inputRef={register()}
+                      />
+                    </Grid>
                   </Grid>
-                  <Grid item xs={6}>
-                    <Button
-                      className={styles.button}
-                      variant="contained"
-                      color="secondary"
-                      type="submit"
-                      onClick={event => {
-                        getCurrentLocation();
-                      }}
-                    >
-                      Get location
-                    </Button>
+
+                  <Typography className={styles.locationInfo}>
+                    Location ID: {locationDetails.id}
+                  </Typography>
+                  <Typography className={styles.locationInfo}>
+                    Location Name: {locationDetails.name}
+                  </Typography>
+                  <Typography className={styles.locationInfo}>
+                    Secret: {locationDetails.secret}
+                  </Typography>
+
+                  <Grid
+                    className={styles.buttonsContainer}
+                    container
+                    spacing={1}
+                    wrap="wrap"
+                  >
+                    <Grid item xs={12}>
+                      <Button
+                        className={styles.button}
+                        variant="contained"
+                        color="secondary"
+                        onClick={getCurrentLocation}
+                        type="submit"
+                      >
+                        Get location
+                      </Button>
+                    </Grid>
                   </Grid>
-                </Grid>
-              </CardContent>
-            </form>
-          </Card>
+                </CardContent>
+              </form>
+            </Card>
+          </Grid>
         </Container>
       </main>
     </div>
